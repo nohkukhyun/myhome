d0080bc707634ca5272673ea1fd656910d458900
nohkukhyun <kh.noh@qualson.com>
2020-06-01 18:36:34 +0900

    ts 수정

--- src/containers/movie/Movie.tsx
+++ src/containers/movie/Movie.tsx
@@ -1,33 +1,30 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getMovieAsyncAction } from "../../store/movie/actions/movieAction";
-import { RootState } from "../../store/rootReducer";
-import { movieDto } from "../../store/movie/types";
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getMovieAsyncAction } from "../../store/movie/actions/movieAction"
+import { RootState } from "../../store/rootReducer"
+import { movieDto } from "../../store/movie/types"
 
-export interface type {
-  item: [] | null;
-}
+export interface type {}
 
 function Movie() {
-  const dispatch = useDispatch();
-  const { list } = useSelector((state: RootState) => state.movies.movie);
-  const item = list;
+  const dispatch = useDispatch()
+  const { list } = useSelector((state: RootState) => state.movies.movie)
 
   useEffect(() => {
-    dispatch(getMovieAsyncAction.request(""));
-  }, [dispatch]);
+    dispatch(getMovieAsyncAction.request(""))
+  }, [dispatch])
 
   useEffect(() => {
-    console.log({ list });
-  });
+    console.log({ list })
+  })
 
   return (
     <div>
       <div>
-        <ul>{item}</ul>
+        <ul></ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default Movie;
+export default Movie
