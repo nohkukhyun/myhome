e295725a4263e1778ad854c8a754dc5903b5802f
nohkukhyun <kh.noh@qualson.com>
2020-06-16 14:27:08 +0900

    reduser updated

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,15 +1,13 @@
-import { combineReducers } from "redux";
-import testReducer from "./github/reducers/testReducer";
-import github from "./github/reducers/githubReducer";
-import movies from "./movie/reducers/movie.reducer";
+import { combineReducers } from "redux"
+import github from "./reducers/githubReducer"
+import movies from "./reducers/movie.reducer"
 
 const rootReducer = combineReducers({
-  testReducer,
   github,
   movies,
-});
+})
 
-export default rootReducer;
+export default rootReducer
 // 루트 리듀서의 반환값를 유추해줍니다
 // 추후 이 타입을 컨테이너 컴포넌트에서 불러와서 사용해야 하므로 내보내줍니다.
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>
